fc741ffd950708d18e948674a778fef632932f47
Saknaorng Sombujtaroan <umsuperza14@gmail.com>
2021-02-12 00:18:47 +0700

    🎉Add index.js

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div style={{ color: `Teal`}}>
+            <h1>About Me</h1>
+        </div>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return <div style={{ color: `purple`, fontSize: `52px`}}>
+  <h1>Hello World</h1>
+  <p>What</p>
+  <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+}
